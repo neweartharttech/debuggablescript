038f570a406546def964e2942f5a8119649c5425
dee-UKI <dee.acharjya.toptal@ultimateknowledge.com>
2024-08-20 20:15:38 -0400

    hub spot api works

---
+++ sync-hubspot/readMe.md
@@ -0,0 +1,9 @@
+# creating hubspot private app
+
+- create private app using settings
+https://developers.hubspot.com/docs/api/private-apps
+
+- needs scopes
+    crm.lists read/write
+    crm.schemas.contacts read/write
+
--- sync-hubspot/src/main.ts
+++ sync-hubspot/src/main.ts
@@ -1,5 +1,22 @@
+import { Client } from "@hubspot/api-client";
+
 const main = async () => {
-  console.log('i am running async 4');
+  console.log('Staring sync');
+
+  const token = process.env.MY_HUBSPOT_TOKEN;
+  if(!token)
+    throw new Error("MY_HUBSPOT_TOKEN env missing")
+
+  const hubspotClient = new Client({ accessToken: token });
+
+
+  const test = await hubspotClient.crm.contacts.basicApi.getPage(10,undefined,[
+    "email","firstname","lastname"
+  ]);
+
+  console.log("Got contact ", JSON.stringify(test.results,null, 2));
+
+  console.log('Done sync');
   process.exit(0);
 };
 
